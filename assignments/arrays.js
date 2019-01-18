275d453bce532045c670e1b42df0a9919e016ace
Ian Be <ian84be@gmail.com>
2019-01-18 17:12:55 -0600

    solve arrays#2

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -72,15 +72,16 @@ const findId = function(idnum) {
     for (obj in inventory) {
       if (inventory[obj].id == idnum) return `Car ${inventory[obj].id} is a ${inventory[obj].car_year} ${inventory[obj].car_make} ${inventory[obj].car_model}`;
     }
-    }
-    console.log(findId(33));
+}
+console.log(findId(33));
 
 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log('toot toot');
+const lastCar = (inventory.length-1);
+console.log(`Last Car is a ${inventory[lastCar].car_make} ${inventory[lastCar].car_model}`)
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
